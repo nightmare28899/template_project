00ff805165fb389165f38098613f3827fbf77151
Luis <luisadriannu13@gmail.com>
2025-06-11 09:28:07 -0600

    Agregando colores y reset de estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,33 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {ConfigProvider, App as AntdApp} from 'antd'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ConfigProvider, App as AntdApp } from "antd";
+import { BrowserRouter } from "react-router-dom";
+import App from "@/App";
 import GifLoader from "@/components/GifLoader";
 
 const theme = {
-    components: {
-        // antd components theme customization
-    },
-}
+  token: {
+    guinda: "#671648",
+    pink: "#FFC3D0",
+    gray: "#F3F4F6",
+    colorSuccess: "#10B981",
+    colorWarning: "#F59E0B",
+    colorError: "#EF4444",
+  },
+  components: {
+    // antd components theme customization
+  },
+};
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <ConfigProvider locale="esEs" theme={theme}>
-            <StrictMode>
-                <AntdApp>
-                    <GifLoader/>
-                    <App/>
-                </AntdApp>
-            </StrictMode>
-        </ConfigProvider>
-    </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ConfigProvider locale="esEs" theme={theme}>
+      <StrictMode>
+        <AntdApp>
+          <GifLoader />
+          <App />
+        </AntdApp>
+      </StrictMode>
+    </ConfigProvider>
+  </BrowserRouter>
+);
